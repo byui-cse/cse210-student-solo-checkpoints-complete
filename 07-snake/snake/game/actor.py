08593825b2fb4992f5382f91fbcd6c2c87660713
Scott Burton <burtons@byui.edu>
2021-10-23 15:52:40 -0600

    Updated methods

--- 07-snake/snake/game/actor.py
+++ 07-snake/snake/game/actor.py
@@ -2,7 +2,8 @@ from game import constants
 from game.point import Point
 
 class Actor:
-    """A visible, moveable thing that participates in the game. The responsibility of Actor is to keep track of its appearance, position 
+    """A visible, moveable thing that participates in the game. The
+    responsibility of Actor is to keep track of its appearance, position 
     and velocity in 2d space.
 
     Stereotype:
@@ -12,6 +13,8 @@ class Actor:
         _text (string): The textual representation of the actor.
         _position (Point): The actor's position in 2d space.
         _velocity (Point): The actor's speed and direction.
+        _width (int): The actor's width
+        _height (int): The actor's height
     """
 
     def __init__(self):
